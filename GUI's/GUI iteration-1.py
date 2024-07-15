9bf62464671dc6b5862874a9d6b82d4cefd463aa
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-07-15 09:28:56 +0100

    adding comments

--- GUI's/GUI iteration-1.py
+++ GUI's/GUI iteration-1.py
@@ -9,27 +9,34 @@ customtkinter.set_default_color_theme("dark-blue")
 
 screen = customtkinter.CTk()
 screen.geometry("500x350")
+#creating the screen size
 
 def login():
     print("Test")
+#Defining the main screen
     
 frame = customtkinter.CTkFrame(master=screen)
 frame.pack(pady=20, padx=60, fill ="both", expand=True)
+#creates a frame which buttons can be placed and moved inside
 
 label = customtkinter.CTkLabel(master=frame, text="Login System")
 label.pack(pady=12, padx=10)
+#title
 
 entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
 #puts 'username' in the box when there's no other text there
 entry1.pack(pady=12, padx=10)
+#boarder
 entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
 #show = "*" encodes the password so no one can see it
 entry2.pack(pady=12, padx=10)
 
 loginB = customtkinter.CTkButton(master=frame, text="Login", command=login)
 loginB.pack(pady=12, padx=10)
+#The button which will move you onto the next page
 
 checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
 checkbox.pack(pady=12, padx=10)
+#to ensure login details are remebered, e.g. on personal devices 
 
 screen.mainloop()
