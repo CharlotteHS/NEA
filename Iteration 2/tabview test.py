12339de47f731c1f16fe0d26e508fb30e7b513d5
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-09-22 09:38:07 +0100

    initialising worked, still have a headache

--- Iteration 2/tabview test.py
+++ Iteration 2/tabview test.py
@@ -1,30 +1,34 @@
+import tkinter
+import tkinter.messagebox
 import customtkinter
-from tkinter import *
-from tkinter.ttk import *
 import customtkinter as ctk
+
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
+class App(ctk.CTk):
+    def __init__(screen):
+        super().__init__()
 
-screen = ctk.CTk()
-screen.title("Login and Registration")
-screen.geometry("500x350")
-master = screen
-    
-frame = customtkinter.CTkFrame(master=screen)
-self = customtkinter.CTkTabview(master=screen)
-frame.pack(pady=20, padx=60, fill ="both", expand=True)
+        #configuring the window and frame layout
+        screen.title("Login and Registration")
+        screen.geometry("500x350")
+        frame = customtkinter.CTkFrame(master=screen)
+        frame.pack(pady=20, padx=60, fill ="both", expand=True)
 
-label = ctk.CTkLabel(master=frame, text="Tabview Test")
-label.pack(pady=12, padx=10)
+        label = ctk.CTkLabel(master=frame, text="Tabview Test")
+        label.pack(pady=12, padx=10)
 
-#creating tabview
-self.tabview = ctk.CTkTabview(frame, width=450)
-self.tabview = self.tabview.add("Homescreen")
+        #creating tabview
+        screen.tabview = ctk.CTkTabview(frame, width=450)
+        screen.tabview.pack(pady=1, padx=1)
+        screen.tabview.add("Homescreen")
 
-self.optionmenu_1 = ctk.CTkOptionMenu(self.tabview.tab("CTkTabview"), dynamic_resizing=False,
-                                      values=["Value 1", "Value 2", "Value Long Long Long"])
+        screen.optionmenu_1 = ctk.CTkOptionMenu(screen.tabview.tab("CTkTabview"), dynamic_resizing=False,
+                                            values=["Value 1", "Value 2", "Value Long Long Long"])
 
 
-screen.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
