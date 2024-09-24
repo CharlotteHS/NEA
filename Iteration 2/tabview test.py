3dc280628e8fe96e998c1405c2243d971eb45f1b
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-09-24 10:24:36 +0100

    tabview working, and the right size

--- Iteration 2/tabview test.py
+++ Iteration 2/tabview test.py
@@ -15,17 +15,17 @@ class App(ctk.CTk):
         screen.title("Login and Registration")
         screen.geometry("500x350")
         frame = customtkinter.CTkFrame(master=screen)
-        frame.pack(pady=20, padx=60, fill ="both", expand=True)
-
-        label = ctk.CTkLabel(master=frame, text="Tabview Test")
-        label.pack(pady=12, padx=10)
+        frame.pack(pady=0, padx=0, fill ="both", expand=True)
 
         #creating tabview
-        screen.tabview = ctk.CTkTabview(frame, width=450)
-        screen.tabview.pack(pady=1, padx=1)
+        screen.tabview = ctk.CTkTabview(frame, width=500, height=350)
+        screen.tabview.pack(pady=0, padx=0)
         screen.tabview.add("Homescreen")
+        screen.tabview.add("Learning")
+        screen.tabview.add("Progress")
 
-        screen.optionmenu_1 = ctk.CTkOptionMenu(screen.tabview.tab("CTkTabview"), dynamic_resizing=False,
+
+        screen.optionmenu_1 = ctk.CTkOptionMenu(screen.tabview.tab("Homescreen"), dynamic_resizing=False,
                                             values=["Value 1", "Value 2", "Value Long Long Long"])
 
 
