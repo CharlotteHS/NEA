9c361eadf9ea604cb6eef7e61fe2065c21243816
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-11-25 21:18:26 +0000

    more file issues

--- Restart/ultra-test.py
+++ Restart/ultra-test.py
@@ -1,2 +1,15 @@
-import ultralytics
-print(ultralytics.__version__)
+#from ultralytics import YOLO
+#import YOLO
+
+#model = YOLO('yolov8n.pt')
+
+#model.train(data='custom_dataset.yaml', epochs=50, imgsz=640)
+
+#yolo task=detect mode=train model=yolov8n.pt data=dataset.yaml epochs=100 imgsz=640 batch=16
+#changed dataset.yaml → dataInfo.yaml to fit our yaml file
+
+#C:\Users\xthec>.\venv\Scripts\activate
+#The system cannot find the path specified.
+
+
+
