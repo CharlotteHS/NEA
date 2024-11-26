955a06c29f38eed2bea192602675c5b39fc79303
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-11-26 11:47:16 +0000

    tryna fix stuff

--- Restart/ultra-test.py
+++ Restart/ultra-test.py
@@ -5,8 +5,7 @@
 
 #model.train(data='custom_dataset.yaml', epochs=50, imgsz=640)
 
-#yolo task=detect mode=train model=yolov8n.pt data=dataset.yaml epochs=100 imgsz=640 batch=16
-#changed dataset.yaml → dataInfo.yaml to fit our yaml file
+#yolo task=detect mode=train model=yolov8n.pt data=dataInfo.yaml epochs=100 imgsz=640 batch=16
 
 #C:\Users\xthec>.\venv\Scripts\activate
 #The system cannot find the path specified.
