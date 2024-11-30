dd5fb1bb248c67e338e0bbdce59f29b6f5ffc167
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-11-30 22:15:47 +0000

    more stuff

--- Restart/ultra-test.py
+++ Restart/ultra-test.py
@@ -16,5 +16,8 @@ val_path = r"C:/Users/xthec/OneDrive/Documents/GitHub/NEA/datasets/Dataset/label
 print(os.listdir(val_path))
 #working
 
-#yolo task=detect mode=val model=runs/detect/train2/weights/best.pt data=dataInfo.yaml
+#yolo task=detect mode=train model=yolov8n.pt data=dataInfo.yaml epochs=100 imgsz=640 batch=16
+#yolo task=detect mode=val model=runs/detect/train3/weights/best.pt data=dataInfo.yaml
 #yolo task=detect mode=predict model=runs/detect/train/weights/best.pt source=Restart/Tensorflow/workspace/testimages
+
+#restart epoch by inputting: yolo task=detect mode=train model=runs/detect/train/weights/last.pt data=dataInfo.yaml epochs=100
