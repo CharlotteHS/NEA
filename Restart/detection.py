56beaa9f132b745914b904f7c4328bf4d8392cc2
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-12-03 20:28:18 +0000

    frame opens and closes

--- Restart/detection.py
+++ Restart/detection.py
@@ -9,6 +9,24 @@ model.export(format='onnx')
 #exporting to ONNX and creating a file
 
 model_path = 'yolov8n.onnx'
+net = cv2.dnn.readNetFromONNX(model_path)
+
+
 
 cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error")
+    exit()
 
+while True:
+    ret, screen = cap.read()
+
+    cv2.imshow("Screen", screen)
+    cv2.waitKey(1)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'): #
+        #shuts the window down when you press q
+        break
+
+cap.release()
+cv2.destroyAllWindows()
