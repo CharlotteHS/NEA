c6b2a3a527bf73e06f237b3626b9850b9e45bae4
CharlotteHS <117395928+CharlotteHS@users.noreply.github.com>
2024-12-08 18:35:26 +0000

    somehow broke it even more

--- Iteration 3/GUI_finalfomr.py
+++ Iteration 3/GUI_finalfomr.py
@@ -1,5 +1,6 @@
 import customtkinter
 from tkinter import *
+import tkinter as tk
 from tkinter.ttk import *
 import customtkinter as ctk
 from tkinter import messagebox
@@ -9,6 +10,7 @@ import time
 from ultralytics import YOLO
 import cv2
 import numpy as np
+from PIL import Image, ImageTk
 
 ctk.set_appearance_mode("dark")
 #this sets the background to dark, light, or system
@@ -129,9 +131,70 @@ def NewWindow():
         #Object Detection Here ↓
         def a_e():
             print("Loading")
-            abcde = ctk.CTkToplevel(screen)
+
+            #inputting the detection.py work here
+            model = YOLO('runs/detect/train3/weights/best.pt')
+            #loading the pretrained model which holds the best ver. of our data
+
+            cap = cv2.VideoCapture(0)
+            if not cap.isOpened():
+                print("Error")
+                exit()
+            #opening webcam
+
+
+            abcde = ctk.CTkToplevel(frame)
             abcde.title("BSL Alphabet: A to E")
-            abcde.geometry("600x400")
+            abcde.geometry("650x450")
+
+            web_frame = ctk.CTkFrame(abcde, width=450, height=350)
+            web_frame.pack(side=ctk.LEFT, padx=12, pady=10)
+            #creating a frame in the window for the webcam to be placed in
+            web_label = ctk.CTkLabel(web_frame)
+            web_label.pack()
+            #labelling the webcam feed
+
+
+            def check_frame():
+                ret, frame = cap.read()
+                if not ret:
+                    print("Fetch Error")
+                    cap.release
+                    return
+
+
+                show = model(frame)
+                #initiates object detection
+
+                annotations = show[0].plot()
+                cv2.imshow("BSL Alphabet: A to E", annotations)
+                #shows detection on the screen
+
+                annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+                img = Image.fromarray(annotated_frame)
+                imgtk = ImageTk.PhotoImage(image=img)
+                #converting frame for display
+                
+                web_label.imgTk = imgtk
+                web_label.configure(image=imgtk)
+                #updating the video label
+
+                abcde.after(10, check_frame)
+                #schedule frame update
+
+            check_frame()
+            abcde.mainloop()
+            #starting loop
+
+            if cv2.waitKey(1) & 0xFF == ord('x'): #
+                #shuts the window down when you press x
+                cap.release()
+                cv2.destroyAllWindows()
+                #shuts the webcam screen down
+                #end of detection.py file
+            
+
+            learn.mainloop()
 
 
         c1 = ctk.CTkButton(learn,text="a_e",command=a_e)
